e9e628cfbf75d4eb210695a114592d38de56eeb6
mahmoudg0 <mahmoud.gamal.15497@gmail.com>
2018-05-06 18:27:51 +0200

    Testing Birge Vieta

--- Roots/OpenMethods/BirgeVieta.py
+++ Roots/OpenMethods/BirgeVieta.py
@@ -1,36 +1,45 @@
+import sympy
+from sympy.parsing import sympy_parser
+import numpy as np
+
 from Roots.OpenMethod import OpenMethod
 
 
 class BirgeVieta(OpenMethod):
-    def __init__(self, function, derivative=None, second_derivative=None, multiplicity=1, error=0.0001
-                 , max_iterations=50, gx=None):
-        super().__init__(function, derivative, second_derivative, multiplicity, error, max_iterations, gx)
+    def __init__(self, function, error=0.0001, max_iterations=50):
+        super().__init__(function, error, max_iterations)
 
     def do(self, xi):
         return self.birge_vieta(xi)
 
     def birge_vieta(self, xi):
         old_root, itr, root, rel = 0, 0, 0, 0
+        data = []
         while itr < self.max_iterations:
             b_list = self.get_b(xi)
             c_list = self.get_c(xi, b_list)
 
-            funct_xi = b_list[0]
-            derivative_xi = c_list[0]
+            funct_xi = b_list[-1]
+            derivative_xi = c_list[-1]
+
             if derivative_xi == 0:
                 raise Exception("Division by zero", root, itr, rel)
-            root = xi - (funct_xi / derivative_xi)
+            root = float(xi - (funct_xi / derivative_xi))
             ea = abs(root - old_root)
             rel = abs(ea / root) * 100
+
+            record = np.array([itr, b_list, c_list, xi, root, ea, rel])
+            data.append(record)
+
             if ea <= self.error:
-                return root, itr, rel
+                return data
             old_root = root
             xi = root
             itr = itr + 1
-        return root, rel, itr
+        return data
 
     def get_b(self, xi):
-        a_list = self.function.coefficients
+        a_list = self.extract_coefficients()
         b = [a_list[0]]
         for i in range(1, len(a_list)):
             b.append(a_list[i] + xi * b[-1])
@@ -41,3 +50,9 @@ class BirgeVieta(OpenMethod):
         for i in range(1, len(b_list) - 1):
             c.append(b_list[i] + xi * c[-1])
         return c
+
+    def extract_coefficients(self):
+        expr = self.function.function
+        polyn = sympy.Poly(expr, sympy.symbols("x"))
+        coeffs = polyn.all_coeffs()
+        return [x / coeffs[0] for x in coeffs]
