f2945916e32164a2ce0c2eb60a123f2a1a29e3e2
mahmoudg0 <mahmoud.gamal.15497@gmail.com>
2018-05-11 18:42:31 +0200

    Gauss Jordan exception

--- LinearEquation/GaussJordan.py
+++ LinearEquation/GaussJordan.py
@@ -17,10 +17,24 @@ class GaussJordan:
             for i in range(rows):
                 if i == k:
                     continue
+                if self.values[k][shift] == 0:
+                    raise Exception("Division by zero")
                 factor = self.values[i][shift]/self.values[k][shift]
                 for j in range(shift, cols):
                     self.values[i][j] -= factor * self.values[k][j]
         values = []
+
+        zero_flag = True
+        for k in self.values[-1, :-1]:
+            if k == 0.0:
+                continue
+            else:
+                zero_flag = False
+                break
+
+        if zero_flag:
+            raise Exception("No unique solution")
+
         for k in range(rows):
             values.append(self.values[k][cols-1] / self.values[k][k])
         return values
